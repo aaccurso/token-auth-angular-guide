f1f47b605ca20c910eef749a22a722799e0fae5a
Alberto Pose <albertopose@gmail.com>
2014-02-13 17:12:14 -0300

    Step 2: /authenticate

--- app.js
+++ app.js
@@ -12,7 +12,20 @@ app.use(express.json({strict: true}));
 app.use('/', express.static('/' + __dirname));
 
 app.post('/authenticate', function (req, res) {
+  var user = req.body;
+  var profile = {
+    firstName: 'John',
+    lastName: 'Doe',
+    mail: 'john@doe.com',
+    id: 1
+  };
 
+  if (!(user.user === 'john.doe' && user.password === '123456')) {
+    res.send(401);
+    return;
+  }
+
+  res.send(200, profile);
 });
 
 app.listen(1337);
--- scripts/myApp.js
+++ scripts/myApp.js
@@ -1,1 +1,20 @@
 var myApp = angular.module('myApp', []);
+
+function LoginController($scope, $http) {
+  $scope.user = '';
+  $scope.password = '';
+  $scope.message = '';
+  $scope.submit = function () {
+    $http.post('/authenticate', {
+      user: $scope.user,
+      password: $scope.password
+    })
+    .success(function (userData) {
+      $scope.message = 'Welcome ' + userData.firstName + ' ' +
+        userData.lastName + '!';
+    })
+    .error(function (error) {
+      alert(error);
+    });
+  };
+}
